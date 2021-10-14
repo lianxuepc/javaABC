8a46a1ff435c476654534ed8d8da93089c81983d
lian <1019510467@qqcomgit config --global user.email>
2021-10-14 22:19:56 +0800

    a

---
+++ javaLearn/base/src/第一部分/数据类型转换.java
@@ -0,0 +1,12 @@
+package 第一部分;
+
+public class 数据类型转换
+{
+    public static void main(String[] args)
+    {
+        int a = 155;
+        double b = 1.23;
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
